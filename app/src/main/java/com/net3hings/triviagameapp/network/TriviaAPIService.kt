aea252037c234ca33050a3f32fc4ced9a5347a2e
Bart Rozworski <bart.rozworski@gmail.com>
2024-01-11 00:43:42 +0100

    Fetching only multiple choice questions

--- app/src/main/java/com/net3hings/triviagameapp/network/TriviaAPIService.kt
+++ app/src/main/java/com/net3hings/triviagameapp/network/TriviaAPIService.kt
@@ -11,7 +11,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 private const val BASE_URL = "https://opentdb.com/"
-private const val QUESTIONS_ENDPOINT = "api.php?amount=10"
+private const val QUESTIONS_ENDPOINT = "api.php?amount=10&type=multiple"
 //private const val QUERY_ENDPOINT = "map_service.html"
 
 private val moshi = Moshi.Builder()
