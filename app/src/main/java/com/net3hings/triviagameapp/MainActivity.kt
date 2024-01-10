8c367feef71a997a4c2cbc28386748389669096b
Bart Rozworski <bart.rozworski@gmail.com>
2024-01-11 00:43:05 +0100

    Disabled the night mode

--- app/src/main/java/com/net3hings/triviagameapp/MainActivity.kt
+++ app/src/main/java/com/net3hings/triviagameapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.net3hings.triviagameapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.net3hings.triviagameapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,8 @@ class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+
+		AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
