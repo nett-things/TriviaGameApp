12392eda9ae1b31aa1e5cd6eb6a97ec7279d8e0d
Bart Rozworski <bart.rozworski@gmail.com>
2024-01-11 02:14:41 +0100

    Added the view binding and a OnClickListener for startGameButton

--- app/src/main/java/com/net3hings/triviagameapp/MenuFragment.kt
+++ app/src/main/java/com/net3hings/triviagameapp/MenuFragment.kt
@@ -5,12 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.net3hings.triviagameapp.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
+	private lateinit var binding: FragmentMenuBinding
+
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
-	): View? {
-		return inflater.inflate(R.layout.fragment_menu, container, false)
+	): View {
+		binding = FragmentMenuBinding.inflate(inflater, container, false)
+
+		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		binding.startGameButton.setOnClickListener {
+			findNavController().navigate(MenuFragmentDirections.actionMenuFragmentToQuestionFragment())
+		}
 	}
 }
