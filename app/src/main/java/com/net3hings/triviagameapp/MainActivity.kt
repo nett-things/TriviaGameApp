c6718b01d9bcff1886510d3963c8597369c27d8c
Bart Rozworski <bart.rozworski@gmail.com>
2024-01-15 05:55:52 +0100

    Added setting the UI theme mode

--- app/src/main/java/com/net3hings/triviagameapp/MainActivity.kt
+++ app/src/main/java/com/net3hings/triviagameapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.net3hings.triviagameapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
 import com.net3hings.triviagameapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +12,23 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
-		AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+		val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+		when(preferences.getString("ui_preference", "1")) {
+			"1" -> setThemeMode(AppCompatDelegate.MODE_NIGHT_NO)
+			"2" -> setThemeMode(AppCompatDelegate.MODE_NIGHT_YES)
+			"3" -> setThemeMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+		}
 
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
+	}
+
+	fun setThemeMode(theme: Int) {
+		when(theme) {
+			AppCompatDelegate.MODE_NIGHT_NO -> AppCompatDelegate.setDefaultNightMode(theme)
+			AppCompatDelegate.MODE_NIGHT_YES -> AppCompatDelegate.setDefaultNightMode(theme)
+			AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM -> AppCompatDelegate.setDefaultNightMode(theme)
+			else -> {}
+		}
 	}
 }
--- app/src/main/java/com/net3hings/triviagameapp/SettingsFragment.kt
+++ app/src/main/java/com/net3hings/triviagameapp/SettingsFragment.kt
@@ -1,11 +1,55 @@
 package com.net3hings.triviagameapp
 
 import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.net3hings.triviagameapp.database.StatisticsViewModel
+
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
+	private val viewModel: StatisticsViewModel by activityViewModels {
+		StatisticsViewModel.StatisticsViewModelFactory(
+			(activity?.application as TriviaGameApplication).repository
+		)
+	}
+
 	override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
 		setPreferencesFromResource(R.xml.preferences, rootKey)
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		// set up the listener for handling the go back button
+		requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+			override fun handleOnBackPressed() {
+				findNavController().popBackStack(R.id.menuFragment, false)
+			}
+		})
+
+		val uiPreference: Preference? = findPreference("ui_preference")
+		uiPreference?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, newValue ->
+			when(newValue.toString()) {
+				"1" -> (activity as MainActivity?)?.setThemeMode(AppCompatDelegate.MODE_NIGHT_NO)
+				"2" -> (activity as MainActivity?)?.setThemeMode(AppCompatDelegate.MODE_NIGHT_YES)
+				"3" -> (activity as MainActivity?)?.setThemeMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+				else -> {}
+			}
+
+			true
+		}
+
+
+		val button: Preference? = findPreference("clear_statistics_preference")
+		button?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+			viewModel.clearAll()
+			true
+		}
 	}
 }
