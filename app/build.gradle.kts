5c227540adb615f803da7a37daadf434a3316495
Bart Rozworski <bart.rozworski@gmail.com>
2024-01-18 17:01:22 +0100

    Added Vico dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,13 @@ dependencies {
 	// Preference
 	implementation("androidx.preference:preference-ktx:1.2.1")
 
+	// Vico
+	implementation("com.patrykandpatrick.vico:compose:1.13.1")
+	implementation("com.patrykandpatrick.vico:compose-m2:1.13.1")
+	implementation("com.patrykandpatrick.vico:compose-m3:1.13.1")
+	implementation("com.patrykandpatrick.vico:core:1.13.1")
+	implementation("com.patrykandpatrick.vico:views:1.13.1")
+
 	// Test implementations
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
