a48e6a1d7d7aa3063ac743384132503e03e734da
Bart Rozworski <bart.rozworski@gmail.com>
2024-01-21 09:00:06 +0100

    Implemented the loading screen and some code clean up

--- app/src/main/java/com/net3hings/triviagameapp/StatisticsFragment.kt
+++ app/src/main/java/com/net3hings/triviagameapp/StatisticsFragment.kt
@@ -1,7 +1,6 @@
 package com.net3hings.triviagameapp
 
 import android.os.Bundle
-import android.text.Html
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +11,8 @@ import androidx.navigation.fragment.findNavController
 import com.net3hings.triviagameapp.database.StatisticsItem
 import com.net3hings.triviagameapp.database.StatisticsViewModel
 import com.net3hings.triviagameapp.databinding.FragmentStatisticsBinding
+import com.patrykandpatrick.vico.core.axis.formatter.DecimalFormatAxisValueFormatter
+import com.patrykandpatrick.vico.core.axis.vertical.VerticalAxis
 import com.patrykandpatrick.vico.core.entry.entryModelOf
 
 class StatisticsFragment : Fragment() {
@@ -25,6 +26,7 @@ class StatisticsFragment : Fragment() {
 
 	private var items: List<StatisticsItem>? = null
 	private var mostPlayedCategory: Int = 0
+	private var mostPlayedCategoryPercent: Double = 0.0
 	private var avgCorrectAnswers: MutableList<Double> = mutableListOf()
 	private var avgScores: MutableList<Double> = mutableListOf()
 	private var avgAnswerTimes: MutableList<Double> = mutableListOf()
@@ -54,55 +56,80 @@ class StatisticsFragment : Fragment() {
 			itemList?.let {
 				items = itemList
 
-				calculateData()
-				displayData()
+				if(items!!.isNotEmpty()) {
+					calculateData()
+					setupCharts()
+					displayData()
+
+					binding.progressBar.visibility = View.GONE
+					binding.dataContainer.visibility = View.VISIBLE
+
+				} else {
+					binding.progressBar.visibility = View.GONE
+					binding.noDataLabel.visibility =  View.VISIBLE
+				}
 			}
 		}
 	}
 
 	private fun calculateData() {
-		if(items != null && items!!.isNotEmpty()) {
-			mostPlayedCategory = items?.map { it.category }?.groupingBy { it }?.eachCount()?.maxBy { it.value }?.key!!
+		mostPlayedCategory = items?.map { it.category }?.groupingBy { it }?.eachCount()?.maxBy { it.value }?.key!!
+		mostPlayedCategoryPercent = items?.map { it.category }?.count { it == mostPlayedCategory }!!.toDouble() / items?.size!!
 
-			val ratiosOfCorrectAnswers = items?.map { it.correctAnswers }!!.zip(items?.map { it.questions }!!) { a, b -> a * 100.0 / b }
-			val answerTimes = items?.map { it.duration }!!.zip(items?.map { it.questions }!!) { a, b -> (a / 1000.0) / b }
+		val ratiosOfCorrectAnswers = items?.map { it.correctAnswers }!!.zip(items?.map { it.questions }!!) { a, b -> a.toDouble() / b }
+		val answerTimes = items?.map { it.duration }!!.zip(items?.map { it.questions }!!) { a, b -> (a / 1000.0) / b }
 
-			for(i in items!!.indices) {
-				avgCorrectAnswers.add(ratiosOfCorrectAnswers.slice(0..i).average())
-				avgScores.add(items?.map { it.score }!!.slice(0..i).average())
-				avgAnswerTimes.add(answerTimes.slice(0..i).average())
-			}
+		for(i in items!!.indices) {
+			avgCorrectAnswers.add(ratiosOfCorrectAnswers.slice(0..i).average())
+			avgScores.add(items?.map { it.score }!!.slice(0..i).average())
+			avgAnswerTimes.add(answerTimes.slice(0..i).average())
+		}
+	}
+
+	private fun setupCharts() {
+		with(binding) {
+			val averageCorrectAnswersChartVerticalAxis = averageCorrectAnswersChart.startAxis as VerticalAxis
+			averageCorrectAnswersChartVerticalAxis.valueFormatter = DecimalFormatAxisValueFormatter("###%")
+			averageCorrectAnswersChart.startAxis = averageCorrectAnswersChartVerticalAxis
+
+			averageCorrectAnswersChart.marker = Helper.makeMarker(requireContext())
+
+
+			val averageScoreChartVerticalAxis = averageScoreChart.startAxis as VerticalAxis
+			averageScoreChartVerticalAxis.valueFormatter = DecimalFormatAxisValueFormatter("###,###")
+			averageScoreChart.startAxis = averageScoreChartVerticalAxis
+
+			averageScoreChart.marker = Helper.makeMarker(requireContext())
+
+
+			val averageAnswerTimeChartVerticalAxis = averageAnswerTimeChart.startAxis as VerticalAxis
+			averageAnswerTimeChartVerticalAxis.valueFormatter = DecimalFormatAxisValueFormatter("###,###.#")
+			averageAnswerTimeChart.startAxis = averageAnswerTimeChartVerticalAxis
+
+			averageAnswerTimeChart.marker = Helper.makeMarker(requireContext())
 		}
 	}
 
 	private fun displayData() {
-		if(items != null && items!!.isNotEmpty()) {
-			binding.mostPlayedCategory.text = Html.fromHtml(getString(
-				R.string.most_played_category_text,
-				Helper.resolveCategory(mostPlayedCategory),
-				100.0
-			), Html.FROM_HTML_MODE_COMPACT)
+		binding.mostPlayedCategory.text = getString(
+			R.string.most_played_category_text,
+			Helper.resolveCategory(mostPlayedCategory)
+		)
+		binding.mostPlayedCategoryPercent.text = getString(
+			R.string.most_played_category_percent_text,
+			mostPlayedCategoryPercent * 100
+		)
 
-			binding.averageCorrectAnswers.text = getString(R.string.average_correct_answers_text, avgCorrectAnswers.last())
-			binding.averageCorrectAnswersChart.setModel(entryModelOf(Helper.convertToListOfFloatEntries(avgCorrectAnswers)))
-			binding.averageCorrectAnswersChart.marker = Helper.makeMarker(requireContext())
+		binding.averageCorrectAnswers.text =
+			getString(R.string.average_correct_answers_text,
+			avgCorrectAnswers.last() * 100
+		)
+		binding.averageCorrectAnswersChart.setModel(entryModelOf(Helper.convertToListOfFloatEntries(avgCorrectAnswers)))
 
-			binding.averageScore.text = getString(R.string.average_score_text, avgScores.last())
-			binding.averageScoreChart.setModel(entryModelOf(Helper.convertToListOfFloatEntries(avgScores)))
-			binding.averageScoreChart.marker = Helper.makeMarker(requireContext())
+		binding.averageScore.text = getString(R.string.average_score_text, avgScores.last())
+		binding.averageScoreChart.setModel(entryModelOf(Helper.convertToListOfFloatEntries(avgScores)))
 
-			binding.averageAnswerTime.text = getString(R.string.average_answer_time_text, avgAnswerTimes.last())
-			binding.averageAnswerTimeChart.setModel(entryModelOf(Helper.convertToListOfFloatEntries(avgAnswerTimes)))
-			binding.averageAnswerTimeChart.marker = Helper.makeMarker(requireContext())
-
-
-		} else {
-			binding.mostPlayedCategoryLabel.visibility = View.INVISIBLE
-			binding.averageCorrectAnswersLabel.visibility = View.INVISIBLE
-			binding.averageScoreLabel.visibility = View.INVISIBLE
-			binding.averageAnswerTimeLabel.visibility = View.INVISIBLE
-
-			binding.noDataLabel.visibility = View.VISIBLE
-		}
+		binding.averageAnswerTime.text = getString(R.string.average_answer_time_text, avgAnswerTimes.last())
+		binding.averageAnswerTimeChart.setModel(entryModelOf(Helper.convertToListOfFloatEntries(avgAnswerTimes)))
 	}
 }
