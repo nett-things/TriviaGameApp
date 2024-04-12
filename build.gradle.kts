65575959e23e4a225f4508fceca050b05d645633
Bart Rozworski <bart.rozworski@gmail.com>
2024-04-12 09:48:36 +0200

    Updated navigation-safe-args dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
 		mavenCentral()
 	}
 	dependencies {
-		classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
+		classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
 	}
 }
 
